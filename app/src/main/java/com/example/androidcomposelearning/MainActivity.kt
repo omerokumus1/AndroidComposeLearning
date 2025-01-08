26acaf529e7e6ed2e43253f24b4abfbb5ac92961
Ömer Okumuş <omerokumus3@gmail.com>
2025-01-09 00:51:42 +0300

    text basics

--- app/src/main/java/com/example/androidcomposelearning/MainActivity.kt
+++ app/src/main/java/com/example/androidcomposelearning/MainActivity.kt
@@ -9,24 +9,90 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.PlatformParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.Hyphens
+import androidx.compose.ui.text.style.LineBreak
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextDirection
+import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.text.style.TextMotion
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
+import androidx.compose.ui.unit.sp
 import com.example.androidcomposelearning.ui.theme.AndroidComposeLearningTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val annotatedStr = AnnotatedString(
+            text = "Hello",
+            paragraphStyle = ParagraphStyle(
+                textAlign = TextAlign.Right,
+                textDirection = TextDirection.Ltr,
+                lineHeight = 10.sp,
+                textIndent = TextIndent(firstLine = 10.sp, restLine = 5.sp),
+                platformStyle = PlatformParagraphStyle(includeFontPadding = true),
+                lineHeightStyle = LineHeightStyle(
+                    alignment = LineHeightStyle.Alignment.Center,
+                    trim = LineHeightStyle.Trim.None
+                ),
+                lineBreak = LineBreak.Paragraph,
+                hyphens = Hyphens.Auto,
+                textMotion = TextMotion.Animated
+            ),
+            spanStyle = SpanStyle(
+                color = Color.Red,
+                fontSize = 24.sp,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Monospace,
+                letterSpacing = 2.sp,
+                textDecoration = TextDecoration.Underline
+                //* others..
+            )
+        )
         setContent {
-            AndroidComposeLearningTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Developer!!!!")
-                }
-            }
+//            Text(annotatedStr)
+            Text(
+                text = "Hello",
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.headlineMedium
+            )
         }
     }
+}
+
+@Composable
+fun SimpleText(modifier: Modifier = Modifier) {
+    Text(
+        "Hello, Android!",
+        color = Color.Red,
+        fontSize = 24.sp,
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.Bold,
+        fontFamily = FontFamily.Monospace,
+        //* letterSpacing = 2sp
+        letterSpacing = TextUnit(2f, TextUnitType.Sp),
+        textDecoration = TextDecoration.Underline,
+        textAlign = TextAlign.Center,
+        overflow = TextOverflow.Ellipsis,
+        maxLines = 2,
+        minLines = 1,
+        softWrap = true,
+        style = TextStyle.Default
+    )
 }
 
 @Composable
